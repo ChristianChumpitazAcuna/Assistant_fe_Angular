9c338bf22970b22764d16121f612910df6c6e862
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2024-06-16 09:55:05 -0500

    branch main was cleaned

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { ChatPageComponent } from './app/page/chat-page/chat-page.component';
-
-bootstrapApplication(ChatPageComponent, appConfig)
-  .catch((err) => console.error(err));
